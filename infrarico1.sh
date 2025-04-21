d9ab43e31d8076cd9f67dfb5a30e2049866447e9
Ricardo Cortinhas <ricardo.cortinhas@icen.ufpa.br>
2025-04-21 13:48:22 -0300

    finalização do arquivo IaC

--- infrarico1.sh
+++ infrarico1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+"Arquivo para automatizar criaçãao de diretórios, grupos, usuários e permissões automáticas no Linux"
+
 echo "Criando diretórios ...."
 
 mkdir /publico
@@ -18,9 +20,11 @@ echo "Criando Usuários ....."
 useradd carlos -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+
 useradd debora -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
 useradd sebastiana -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
 useradd roberto -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+
 useradd josefina -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
@@ -30,7 +34,6 @@ echo "Especificando permissões dos diretórios ...."
 chown root:GRP_ADM /adm
 chown root:GPR_VEN /ven
 chown root:GRP_SEC /sec
-
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
